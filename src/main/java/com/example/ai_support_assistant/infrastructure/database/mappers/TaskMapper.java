20de745582a87708883cbe0826a329d337910f0c
ISTIFANO <elamiriamir7@gmail.com>
2025-10-18 14:38:43 +0200

    feat: update database configuration and remove unused service interfaces

--- src/main/java/com/example/ai_support_assistant/infrastructure/database/mappers/TaskMapper.java
+++ src/main/java/com/example/ai_support_assistant/infrastructure/database/mappers/TaskMapper.java
@@ -1,14 +1,2 @@
 package com.example.ai_support_assistant.infrastructure.database.mappers;
 
-import com.dashy.orchestrator.domain.model.task.TaskDto;
-import org.camunda.bpm.engine.task.Task;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-
-@Mapper(componentModel = "spring")
-public interface TaskMapper {
-    @Mapping(target = "id", source = "id")
-    @Mapping(target = "taskInfo", source = "task")
-    TaskDto taskToTaskDTO(Task task);
-}
-
