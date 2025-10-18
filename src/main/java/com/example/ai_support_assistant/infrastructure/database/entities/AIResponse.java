56a75038fd578473b8b71e7b7f94a494dba727cf
ISTIFANO <elamiriamir7@gmail.com>
2025-10-18 15:30:29 +0200

    feat: add database entity classes for AIResponse, Conversation, FAQ, Message, and User

---
+++ src/main/java/com/example/ai_support_assistant/infrastructure/database/entities/AIResponse.java
@@ -0,0 +1,23 @@
+package com.example.ai_support_assistant.infrastructure.database.entities;
+
+import com.example.ai_support_assistant.infrastructure.database.entities.commun.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class AIResponse extends BaseEntity {
+    @ManyToOne
+    private Conversation conversation;
+
+    @Column(columnDefinition = "TEXT")
+    private String reponse; // réponse générée par AI
+
+    private double scoreConfiance;
+    private LocalDateTime dateGeneration = LocalDateTime.now();
+}
