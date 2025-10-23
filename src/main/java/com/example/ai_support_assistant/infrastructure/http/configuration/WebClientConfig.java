b69c8043d7777af96b82b856a1346c28f8086b62
ISTIFANO <elamiriamir7@gmail.com>
2025-10-24 01:11:55 +0200

    fix : config env

--- src/main/java/com/example/ai_support_assistant/infrastructure/http/configuration/WebClientConfig.java
+++ src/main/java/com/example/ai_support_assistant/infrastructure/http/configuration/WebClientConfig.java
@@ -12,6 +12,7 @@ import org.springframework.http.MediaType;
 public class WebClientConfig {
 
     Dotenv dotenv = Dotenv.load();
+
     private final String apiKey = dotenv.get("API_KEY");
 
     @Value("${gemini.model:gemini-2.5-pro}")
